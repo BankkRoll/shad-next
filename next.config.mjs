b058ef6273bc102eb854432da9764b5307cd9feb
Bankk <106103625+BankkRoll@users.noreply.github.com>
2024-06-23 23:09:34 -0400

    init

---
+++ next.config.mjs
@@ -0,0 +1,20 @@
+import createMDX from "@next/mdx";
+import remarkGfm from "remark-gfm";
+
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  swcMinify: true,
+  pageExtensions: ["js", "jsx", "mdx", "ts", "tsx"],
+  // Add any other Next.js config here
+};
+
+const withMDX = createMDX({
+  // Add markdown plugins here, as desired
+  options: {
+    remarkPlugins: [remarkGfm],
+    rehypePlugins: [],
+  },
+});
+
+export default withMDX(nextConfig);
